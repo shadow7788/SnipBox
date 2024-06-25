8e313481d1b43607cbeb5f96f09d0e0c2aa9e2eb
nidhin <nidhintk787@gmail.com>
2024-06-25 21:33:30 +0530

    Login API

--- SnipBox/accounts/urls.py
+++ SnipBox/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, LoginView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view()),
+    path("login/", LoginView.as_view()),
+
 ]
--- SnipBox/accounts/views.py
+++ SnipBox/accounts/views.py
@@ -1,8 +1,9 @@
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.serializers import UserRegisterSerializer
 
@@ -26,3 +27,25 @@ class UserRegisterView(APIView):
             return Response({"message": "Required fields missing"}, status=status.HTTP_400_BAD_REQUEST)
 
 
+class LoginView(APIView):
+
+    def post(self, request):
+        try:
+            user = authenticate(username=request.data["username"], password=request.data["password"])
+            if user is not None:
+                login(request, user)
+                refresh = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    {"message": "Invalid credentials"},
+                    status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return Response({"message": "Required fields missing"}, status=status.HTTP_400_BAD_REQUEST)
